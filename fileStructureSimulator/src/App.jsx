fd07342eaf0ba2bd281cf588f15b7c20dc3364fc
Kris Bowman <129105342+kris26658@users.noreply.github.com>
2025-10-14 08:10:52 -0700

    File Structure Corrections

---
+++ fileStructureSimulator/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1>File Structure Simulator</h1>
+        {/* Add your components and logic here */}
+      </div>
+    </>
+  )
+}
+
+export default App
